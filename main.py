b9bf247a5778c54cf02e92f21295df6c2ed1416b
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-10-09 10:07:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ try:
     import clipboard
 except:
     os.system('pip install clipboard')
-
+try:
+    from termcolor import colored
+except:
+    os.system('pip install termcolor')
 data = []
+banner = '''
+   _         _       _____          _     ___                  
+  /_\  _   _| |_ ___/__   \_____  _| |_  / __\___  _ __  _   _ 
+ //_\\| | | | __/ _ \ / /\/ _ \ \/ / __|/ /  / _ \| '_ \| | | |
+/  _  \ |_| | || (_) / / |  __/>  <| |_/ /__| (_) | |_) | |_| |
+\_/ \_/\__,_|\__\___/\/   \___/_/\_\\__\____/\___/| .__/ \__, |
+                                                  |_|    |___/ 
+'''
 def main(text,lemet):
+    
     try:
         lemet = int(lemet)
     except ValueError:
-        print("\nPlease enter a valid number for 'lemet'")
+        print(colored("\nPlease enter a valid number for 'lemet'",'red'))
         return
 
     print()
     for i in range(int(lemet)):
         
-        print(text+'\n')
+        print(colored(text+'\n','red'))
         data.append(text)
 
     clipboard.copy('\n'.join(data))
@@ -27,6 +39,7 @@ def main(text,lemet):
     
         
 if __name__ == '__main__':
-    user_text = input("\nEnter text which you want to type many time: ")
-    user_lemet = input("\nEnter howmany line you want: ")
+    print(colored(banner,'red'))
+    user_text = input(colored("\nEnter text which you want to type many time: ",'red'))
+    user_lemet = input(colored("\nEnter howmany line you want: ",'red'))
     main(user_text,user_lemet)
